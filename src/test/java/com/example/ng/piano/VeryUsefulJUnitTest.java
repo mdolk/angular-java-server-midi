c2273d9c0514287c7245c3ccc10f610c0b9f6354
Mårten Dolk <marten.dolk@visionael.com>
2011-07-06 13:53:39 +0200

    Added a sample JUnit test to run during build

---
+++ src/test/java/com/example/ng/piano/VeryUsefulJUnitTest.java
@@ -0,0 +1,12 @@
+package com.example.ng.piano;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class VeryUsefulJUnitTest {
+	
+	@Test
+	public void hello() {
+		assertEquals(3, 1+2);
+	}
+}
